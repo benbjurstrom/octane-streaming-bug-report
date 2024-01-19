52ff4a9141f57d708c80680fff27f6198900ae26
Ben Bjurstrom <benbjurstrom@users.noreply.github.com>
2024-01-18 20:58:07 -0800

    Reproduce Error

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use OpenAI\Laravel\Facades\OpenAI;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/chat', function () {
+    $stream = OpenAI::chat()->createStreamed([
+        "model" => "gpt-3.5-turbo",
+        'messages' => [
+            ['role' => 'user', 'content' => 'PHP is '],
+        ],
+    ]);
+    return response()->stream(function () use ($stream) {
+        foreach ($stream as $response) {
+            echo $response->choices[0]->delta->content;
+            flush();
+        }
+    },
+        200,
+        ['X-Accel-Buffering' => 'no']);
 });
